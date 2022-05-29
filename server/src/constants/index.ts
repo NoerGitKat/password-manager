ce62ba131faa97800023a69960cb51ee97140e75
Noer Paanakker <noer@lightworksweb.nl>
2022-05-29 20:38:39 +0200

    replace mongo uri env variable

--- server/src/constants/index.ts
+++ server/src/constants/index.ts
@@ -1,6 +1,4 @@
 export const PORT = process.env.PORT || 4000;
-export const MONGO_URI =
-  process.env.MONGO_URI ||
-  "";
+export const MONGO_URI = process.env.MONGO_URI || "";
 export const SIGNALS = ["SIGTERM", "SIGINT"];
 export const CORS_ORIGIN = process.env.CORS_ORIGIN || "http://localhost:3000";
