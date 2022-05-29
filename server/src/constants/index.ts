138677a5393d98b667005754e93bfde8bf323ddc
Noer Paanakker <noer@lightworksweb.nl>
2022-05-29 20:38:01 +0200

    protect mongo uri

--- server/src/constants/index.ts
+++ server/src/constants/index.ts
@@ -1,6 +1,6 @@
 export const PORT = process.env.PORT || 4000;
 export const MONGO_URI =
   process.env.MONGO_URI ||
-  "mongodb+srv://root:root@cluster0.3nhc5.mongodb.net/?retryWrites=true&w=majority";
+  "";
 export const SIGNALS = ["SIGTERM", "SIGINT"];
 export const CORS_ORIGIN = process.env.CORS_ORIGIN || "http://localhost:3000";
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import { PORT } from "./constants";
 import { connectDB, createServer } from "./utils";
 
@@ -8,6 +9,7 @@ const main = async () => {
     await app.listen(PORT, "0.0.0.0");
     await connectDB(app);
   } catch (error) {
+    console.error("Error!", error);
     process.exit(1);
   }
 };
